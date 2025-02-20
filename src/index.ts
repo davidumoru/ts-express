f95a5d1baeeb59c197371c3a5207313268ce628d
David Umoru <umorudavido@gmail.com>
2025-02-20 18:10:32 +0100

    Refactor server configuration and update package details

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import glowr from 'glowr';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(glowr(`⚡️[server]: Server is running at http://localhost:${port}`, `bg.green`));
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
